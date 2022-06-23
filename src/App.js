c17ff1b9b1161073736365bc9ba50bfbdfc3ee4b
Emir Usubaliev <emirusubaliev@Emirs-MacBook-Pro.local>
2022-06-23 17:45:08 -0500

    lesson 94

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import React, { Component } from 'react';
 import AboutMe from './components/AboutMe/aboutme';
+import { Suspense } from 'react';
 import Footer from './components/Footer/footer';
 import Home from './components/Home/home';
 import UsersContainer from './components/Users/UsersContainer';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
+//import DialogsContainer from './components/Dialogs/DialogsContainer';
 import { Route, Routes } from 'react-router-dom';
 import ReviewsContainer from './components/Reviews/reviewsContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
+//import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
 import { initializeApp } from './redux/app-reducer';
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import Preloader from './components/common/Preloader/Preloader';
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 
 class App extends Component {
@@ -23,22 +27,24 @@ class App extends Component {
 
   render() {
     if (!this.props.initialized) {
-      return <Preloader/>
+      return <Preloader />
     }
     return (
       <div className="App">
         <HeaderContainer />
-        <Routes>
-          <Route path='/dialogs/*' element={<DialogsContainer />} />
-          <Route path='/' element={<Home />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/aboutme' element={<AboutMe />} />
-          <Route path='/reviews/*' element={<ReviewsContainer />} />
-          <Route path='/users' element={<UsersContainer />} />
-          <Route path='/profile/:userId' element={<ProfileContainer />} />
-          <Route path='/profile' element={<ProfileContainer />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
+        <Suspense fallback={<Preloader />}>
+          <Routes>
+            <Route path='/dialogs/*' element={ <DialogsContainer />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/aboutme' element={<AboutMe />} />
+            <Route path='/reviews/*' element={<ReviewsContainer />} />
+            <Route path='/users' element={<UsersContainer />} />
+            <Route path='/profile/:userId' element={ <ProfileContainer />} />
+            <Route path='/profile' element={<ProfileContainer />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </div>
     );
@@ -49,4 +55,4 @@ const mapStateToProps = (state) => ({
   initialized: state.app.initialized
 })
 
-export default connect(mapStateToProps, {initializeApp}) (App) ;
+export default connect(mapStateToProps, { initializeApp })(App);
--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from "./Paginator.module.css";
 
 
-let Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
+let Paginator = ({ totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10 }) => {
 
     let pagesCount = Math.ceil(totalItemsCount / pageSize);
 
@@ -18,20 +18,22 @@ let Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portion
 
 
     return <div className={styles.paginator}>
-        { portionNumber > 1 &&
-        <button className={styles.button} onClick={() => { setPortionNumber(portionNumber - 1) }}>PREV</button> }
+        {portionNumber > 1 &&
+            <button className={styles.button} onClick={() => { setPortionNumber(portionNumber - 1) }}
+            >PREV</button>}
 
-            {pages
-                .filter(p => p >= leftPortionPageNumber && p<=rightPortionPageNumber)
-                .map((p) => {
-                return <span className={ ({
+        {pages
+            .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+            .map((p) => {
+                return <span className={({
                     [styles.selectedPage]: currentPage === p
                 }, styles.pageNumber)}
-                             key={p}
-                onClick={(e) => {onPageChanged(p) }}>{p}</span>
-        })}
-   { portionCount > portionNumber &&
-            <button className={styles.button} onClick={() => { setPortionNumber(portionNumber + 1) }}>NEXT</button> }
+                    key={p}
+                    onClick={(e) => { onPageChanged(p) }}>{p}</span>
+            })}
+        {portionCount > portionNumber &&
+            <button className={styles.button} onClick={() => { setPortionNumber(portionNumber + 1) }}
+            >NEXT</button>}
 
 
     </div>
